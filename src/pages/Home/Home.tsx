fcec3ee7fcd1a0760f797b8fefa9109c0ae9df50
Kauã Werle <kauawerle19@gmail.com>
2022-06-27 15:50:46 -0300

    fix logo

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -16,7 +16,7 @@ const Home: React.FC = () => {
   return (
     <Container> 
     <LogoBox>
-      <Logo src={'assets/logo/Logo.png'} />      
+      <Logo src={'assets/logo/logo.png'} />      
     </LogoBox>
     
     <Button onClick={() => {handleNavigateListPerso()}}>
