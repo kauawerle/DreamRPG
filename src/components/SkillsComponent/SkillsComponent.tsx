ce2364801894b1ab453ab89cc99058598f425e52
Kauã Werle <kauawerle19@gmail.com>
2022-06-30 14:55:57 -0300

    create a compone nt Skills

--- src/components/SkillsComponent/SkillsComponent.tsx
+++ src/components/SkillsComponent/SkillsComponent.tsx
@@ -1,13 +1,18 @@
 import { IonIcon, IonImg } from "@ionic/react";
 import { Logo, LogoBox } from "../LogoBox/styles";
 import shield from './aaa.svg'
-import { Container } from "./style";
+import { CalcBox, Container, InputSkill } from "./style";
 
 const SkillComponent: React.FC = () => {
 	
 	return (
-		<Container>
-			
+		<Container style={{marginTop:"50px"}}>
+			<InputSkill placeholder="Força" type="number"></InputSkill>
+			<InputSkill placeholder="Destreza" type="number"></InputSkill>
+			<InputSkill placeholder="Constituição" type="number"></InputSkill>
+			<InputSkill placeholder="Inteligência" type="number"></InputSkill>
+			<InputSkill placeholder="Sabedoria" type="number"></InputSkill>
+			<InputSkill placeholder="Carisma" type="number"></InputSkill>
 		</Container>
 	);
 };
--- src/components/SkillsComponent/style.ts
+++ src/components/SkillsComponent/style.ts
@@ -5,8 +5,26 @@ export const Container = styled.div`
 	width: 100%;
 	display: flex;
 	flex-direction: row;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-wrap: wrap;
+`;
+export const CalcBox = styled.div`
+	width: 30%;
+	background-color:#423F4D;
+	height: 40px;
+	margin: 10px;
+	border-radius: 4px;
 `;
 
 export const InputSkill =   styled.input`
-
+	background-color:#423F4D;
+	width: 40%;
+	height: 55px;
+	margin: 10px;
+	outline: 0;
+	border: none;
+	box-shadow: 0px 4px 4px rgb(0 0 0 / 25%);
+	text-align: center;
 `;
