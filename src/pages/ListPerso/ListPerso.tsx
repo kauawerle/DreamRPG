39f8ffeb2855a4a2dc27613cdf4e7ab6c2fd4cba
AlanGabriL <alangabrielmakus901@gmail.com>
2022-07-22 15:53:55 -0300

    Add Design Person List

--- src/pages/ListPerso/ListPerso.tsx
+++ src/pages/ListPerso/ListPerso.tsx
@@ -1,23 +1,27 @@
-import { Container, Logo } from '../../theme/global-styles';
-import HeaderComponent from '../../components/header/Header';
-import { LogoBox } from '../CreatePerso/style';
-
+import { Container, Logo } from "../../theme/global-styles";
+import HeaderComponent from "../../components/header/Header";
+import { LogoBox } from "../CreatePerso/style";
+import { DivPerson, PhotoPerson, DivText, Title, SubTitle } from "./style";
 
 const ListPerso: React.FC = () => {
 	return (
 		<>
-			<HeaderComponent/>
-			
+			<HeaderComponent />
 			<Container style={{ marginTop: "50px" }}>
 				<LogoBox>
-					<Logo src={'assets/logo/Logo.png'} />
+					<Logo src={"assets/logo/Logo.png"} />
 				</LogoBox>
-
-				
+				<DivPerson>
+					<PhotoPerson></PhotoPerson>
+					<DivText>
+						<Title>nada</Title>
+						<SubTitle>nada</SubTitle>
+						<SubTitle>nada</SubTitle>
+					</DivText>
+				</DivPerson>
 			</Container>
-
 		</>
-		)
-}
+	);
+};
 
 export default ListPerso;
--- src/pages/ListPerso/style.ts
+++ src/pages/ListPerso/style.ts
@@ -1,1 +1,38 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+
+export const DivPerson = styled.div`
+	display: flex;
+	width: 327px;
+	height: 112px;
+	background-color: #423f4d;
+	box-shadow: 0 4px 4px rgba(0, 0, 0, 0.4);
+	align-items: center;
+`;
+
+export const PhotoPerson = styled.div`
+	margin-left: 11px;
+	margin-right: 18px;
+	width: 72px;
+	height: 72px;
+	border-radius: 50%;
+	background: gray;
+`;
+
+export const DivText = styled.div`
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	text-align: left;
+`;
+
+export const Title = styled.div`
+	font-size: 18px;
+	margin-top: 18px;
+	margin-bottom: 13px;
+`;
+
+export const SubTitle = styled.div`
+	font-size: 12px;
+	color: #999591;
+	margin-bottom: 5px;
+`;
